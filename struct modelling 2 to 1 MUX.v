21092f22b24e17a00a3476e1c61cfa5451168779
Rahulmkv7 <151156907+Rahulmkv7@users.noreply.github.com>
2025-10-02 19:53:02 +0530

    Update struct modelling 2 to 1 MUX.v

--- struct modelling 2 to 1 MUX.v
+++ struct modelling 2 to 1 MUX.v
@@ -3,9 +3,21 @@ module s_2to1_MUX(in,sel,out);
     input sel;
     output out;
     wire t1,t2,t3;
-    NOT G1(t1,sel);
-    AND G2(t2,in[0],t1);
-    AND G3(t3,in[1],sel);
-    OR G4(out,t2,t3);
+    not G1(t1,sel);
+    and G2(t2,in[0],t1);
+    and G3(t3,in[1],sel);
+    or G4(out,t2,t3);
+    
+endmodule
+
+
+module 4to1_MUX(in,sel,out);
+input[3:0]in;
+    input[1:0] sel;
+    output out;
+    wire[1:0]t;
+    s_2to1_MUX M0(in[1:0],sel[0],t[0]);
+    s_2to1_MUX M1(in[3:2],sel[0],t[1]);
+    s_2to1_MUX M2(t,sel[1],out);
     
 endmodule
